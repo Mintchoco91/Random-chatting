0af844728044f5537cc44d3fa7d6b8424dc985eb
DESKTOP-PA044E1\Kiwon <boy0221@nate.com>
2022-09-29 23:53:51 +0900

    socketIO 전송 기능 추가 안드로이드에 채팅 보여주는부분은 아직 미구현

--- app/src/main/java/com/kj/random_chatting/userChatting/UserChattingFragment.java
+++ app/src/main/java/com/kj/random_chatting/userChatting/UserChattingFragment.java
@@ -1,64 +1,27 @@
 package com.kj.random_chatting.userChatting;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.kj.random_chatting.R;
 import com.kj.random_chatting.databinding.FragmentUserChattingBinding;
 import com.kj.random_chatting.databinding.UserRegistActivityBinding;
+import com.kj.random_chatting.userRegist.UserRegistService;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UserChattingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class UserChattingFragment extends Fragment {
     private FragmentUserChattingBinding binding;
+    private static final String TAG = "UserChattingFragment";
+    private Context context;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public UserChattingFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UserMatchingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UserChattingFragment newInstance(String param1, String param2) {
-        UserChattingFragment fragment = new UserChattingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private UserChattingService userChattingService;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -69,8 +32,41 @@ public class UserChattingFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        Log.d(TAG, "Log : " + TAG + " -> onViewCreated");
+        super.onViewCreated(view, savedInstanceState);
+        initializeView();
+        setListener();
+    }
+
+    @Override
     public void onDestroyView(){
         super.onDestroyView();
         binding = null;
+    }
+
+
+    private void initializeView() {
+        Log.d(TAG, "Log : " + TAG + " -> initializeView");
+        context = getContext();
+
+        userChattingService = new UserChattingService(context, binding);
+    }
+
+    private void setListener() {
+        Log.d(TAG, "Log : " + TAG + " -> setListener");
+        View.OnClickListener Listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.fragment_user_chatting_btn_send:
+                        String chatMessage = binding.fragmentUserChattingEtMessage.getText().toString();
+                        userChattingService.btnSendClick(chatMessage);
+                        break;
+                }
+            }
+        };
+
+        binding.fragmentUserChattingBtnSend.setOnClickListener(Listener);
     }
 }
