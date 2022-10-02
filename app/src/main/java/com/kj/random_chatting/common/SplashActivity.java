9afba133382e4ff2efc6bcd1b3765809bb1c84b9
junho <ljuho@naver.com>
2022-10-02 22:29:55 +0900

    로그인 기능 - jwt 인증 방식 적용중

--- app/src/main/java/com/kj/random_chatting/common/SplashActivity.java
+++ app/src/main/java/com/kj/random_chatting/common/SplashActivity.java
@@ -3,11 +3,13 @@ package com.kj.random_chatting.common;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
 import com.kj.random_chatting.R;
 import com.kj.random_chatting.login.LoginActivity;
+import com.kj.random_chatting.userList.UserListActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -19,7 +21,15 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                Intent intent;
+                SharedPreferences prefs = getSharedPreferences("token_prefs", MODE_PRIVATE);
+                String token = prefs.getString("token", null );
+                if (token == null) {
+                    intent = new Intent(SplashActivity.this, LoginActivity.class);
+                } else {
+                    intent = new Intent(SplashActivity.this, UserListActivity.class);
+                }
+
                 startActivity(intent);
                 finish();
             }
--- app/src/main/java/com/kj/random_chatting/login/LoginActivity.java
+++ app/src/main/java/com/kj/random_chatting/login/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.kj.random_chatting.login;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -67,9 +68,16 @@ public class LoginActivity extends AppCompatActivity {
             public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
                     LoginResponse result = response.body();
-                    int resultCode = result.getStatus();
+                    int resultCode = result.getCode();
 
                     if (resultCode == 200) {
+                        // 토큰을 저장한다.
+                        SharedPreferences prefs = getSharedPreferences("token_prefs", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = prefs.edit();
+                        String token = result.getToken();
+                        editor.putString("token", token);
+                        editor.commit();
+
                         Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_LONG).show();
                     } else {
                         Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_LONG).show();
