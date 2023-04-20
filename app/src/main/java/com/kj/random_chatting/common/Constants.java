b3304b4adbf1c94986674d437bd45ac6259ff3cf
DESKTOP-PA044E1\Kiwon <boy0221@nate.com>
2023-04-20 18:48:37 +0900

    regist_phone_text_code_activity 디자인 추가. 및 그외 페이지 안보이던 버튼색 변경

--- app/src/main/java/com/kj/random_chatting/common/Constants.java
+++ app/src/main/java/com/kj/random_chatting/common/Constants.java
@@ -1,7 +1,7 @@
 package com.kj.random_chatting.common;
 
 public final class Constants {
-    public static final String	AWS_IP = "http://13.231.241.115";
+    public static final String	AWS_IP = "http://35.78.188.89";
     public static final String	AWS_PORT = "3000";
     public static final String	AWS_PHP_SERVER_IP_ADDRESS = AWS_IP + "/random-chatting-server/web/";
     public static final String	AWS_NODE_SERVER_IP_ADDRESS = AWS_IP + ":" + Constants.AWS_PORT;
